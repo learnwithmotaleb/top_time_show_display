6b0f3f0eb0fe3cc6b93014b5fd62947f5055607a
Abdul Motaleb <71353762+learnwithmotaleb@users.noreply.github.com>
2024-02-21 02:57:55 +0600

    Initial commit

---
+++ lib/controller.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+import 'package:get/get_state_manager/src/simple/get_controllers.dart';
+import 'package:intl/intl.dart';
+
+class Controller extends GetxController {
+
+
+
+
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'HomeScreen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Top Clock App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+        useMaterial3: false,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
+
+
